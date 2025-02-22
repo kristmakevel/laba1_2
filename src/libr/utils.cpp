841cb2d879bbf99439b123bf86b936303b80938f
kristmakevel <kristmakevel@gmail.com>
2025-02-22 13:38:17 +0300

    feat: Add division function to utils

--- src/libr/utils.cpp
+++ src/libr/utils.cpp
@@ -9,3 +9,12 @@ int subtract(const int& a, const int& b) {
 int multiply(const int& a, const int& b) {
     return a * b;
 }
+
+int division(const int& a, const int& b) {
+    if (b != 0) {
+        return a / b;
+    }
+    else {
+        return 0;
+    }
+}
--- src/libr/utils.hpp
+++ src/libr/utils.hpp
@@ -1,3 +1,4 @@
 int add(const int& a, const int& b);
 int subtract(const int& a, const int& b);
 int multiply(const int& a, const int& b);
+int division(const int& a, const int& b);
